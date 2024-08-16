064a306d803578d5753dfde2aac43e40416f2d04
FangYuan <unmatched12345@outlook.com>
2024-08-16 22:46:28 +0800

    First
    Signed-off-by: FangYuan <unmatched12345@outlook.com>

---
+++ sky-common/src/main/java/com/sky/constant/StatusConstant.java
@@ -0,0 +1,13 @@
+package com.sky.constant;
+
+/**
+ * 状态常量，启用或者禁用
+ */
+public class StatusConstant {
+    //设置状态1 表示正常  0表示锁定  停售那些状态通用
+    //启用
+    public static final Integer ENABLE = 1;
+
+    //禁用
+    public static final Integer DISABLE = 0;
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,93 @@
+package com.sky.service.impl;
+
+import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
+import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+import com.sky.exception.AccountLockedException;
+import com.sky.exception.AccountNotFoundException;
+import com.sky.exception.PasswordErrorException;
+import com.sky.mapper.EmployeeMapper;
+import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    /**
+     * 员工登录
+     *
+     * @param employeeLoginDTO
+     * @return
+     */
+    public Employee login(EmployeeLoginDTO employeeLoginDTO) {
+        String username = employeeLoginDTO.getUsername();
+        String password = employeeLoginDTO.getPassword();
+
+        //1、根据用户名查询数据库中的数据
+        Employee employee = employeeMapper.getByUsername(username);
+
+        //2、处理各种异常情况（用户名不存在、密码不对、账号被锁定）
+        if (employee == null) {
+            //账号不存在
+            throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
+        }
+
+        //密码比对
+
+        password= DigestUtils.md5DigestAsHex(password.getBytes());
+
+        if (!password.equals(employee.getPassword())) {
+            //密码错误
+            throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
+        }
+
+        if (employee.getStatus() == StatusConstant.DISABLE) {
+            //账号被锁定
+            throw new AccountLockedException(MessageConstant.ACCOUNT_LOCKED);
+        }
+
+        //3、返回实体对象
+        return employee;
+    }
+    /**
+     *
+     * 新增员工
+     * @param employeeDTO
+     * @return void
+     * @author gangzi
+     * @create 2024/8/16
+     **/
+
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        //传进来DTO是为了方便封装前端提交的数据
+        //调用持久层Mapper
+        //此处需要将DTO转为实体
+       Employee employee= new Employee();
+       //employee.setName(employeeDTO.getName());
+        //太繁琐
+        //使用Spring提供的BeanUtils来一键拷贝属性-->条件是DTO中的属性名和employee之中必须一样
+        BeanUtils.copyProperties(employeeDTO,employee);
+        //拷贝DTO到实体之中
+        //employeeDTO没有的属性自己设置
+
+        //设置账号的状态1 表示正常  0表示锁定
+        employee.setStatus(StatusConstant.DISABLE);
+
+        //设置密码(dto无得属性
+        //新账号的进入必须设置默认密码
+        //建立一个常量类
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+    }
+
+}
